5785f486e840a31584501ff3b37b62726d7cb822
Slawomir <gliba@wp.pl>
2020-02-03 21:45:03 +0100

    Initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Second line of code");
+        System.out.println("Third line of code");
+        System.out.println("New shortcut");
+        System.out.println("New 2");
+
+    }
+}
