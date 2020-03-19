286de2abe80874f837e8edf8930dda9c120e165a
Slawomir <gliba@wp.pl>
2020-03-19 22:27:12 +0100

    add zmienneDodatek

---
+++ src/co_zmienneDodatek/zmienneDodatek.java
@@ -0,0 +1,15 @@
+package co_zmienneDodatek;
+
+public class zmienneDodatek {
+    public static void main(String[] args){
+        int nazwaZmiennej = 12;
+        int a;
+        int b;
+        int c = 6;
+
+        int d = 3, e, f, nazwaZmiennejDwa = 12;
+
+        a = 12; // zmiana wartosc zmiennej
+        nazwaZmiennej = 100; // zmiana wartosci zmiennej
+    }
+}
