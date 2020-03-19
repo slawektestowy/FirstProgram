61d2acf19598565102351e4c5efa7441acb236b9
Slawomir <gliba@wp.pl>
2020-03-19 22:44:45 +0100

    add wartosciDomyslne

---
+++ src/co_typeProsteWartosciDomyslne/wartosciDomyslne.java
@@ -0,0 +1,22 @@
+package co_typeProsteWartosciDomyslne;
+
+
+public class wartosciDomyslne {
+    public static int domyslnyInt;
+    public static char domChar;
+    public static void main(String[] args){
+        /*
+        byte   0
+        short  0
+        int    0
+        long   0L
+        float  0.0f
+        double 0.0d
+        char   '\u0000'
+        boolean false
+         */
+
+        System.out.println("Domyslny integer: " + domyslnyInt);
+        System.out.println("Domyslny char: " + domChar);
+    }
+}
