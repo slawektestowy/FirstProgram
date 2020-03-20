1a90108808e967d518135c604d24bf81929f1774
Slawomir <gliba@wp.pl>
2020-03-20 09:58:46 +0100

    add typyProsteKonwertowanie

---
+++ src/co_PrimitiveTypes/typyProsteKonwertowanie.java
@@ -0,0 +1,13 @@
+package co_PrimitiveTypes;
+
+public class typyProsteKonwertowanie {
+    public static void main(String[] args){
+        int a = 6;
+        double b = 2.34;
+        System.out.println(a / b);
+
+        byte by = 1000;
+        int ii = by;
+        // byte by1 = ii;  //integer moze byc bajtem, ale bajt nie moze byc integerem
+    }
+}
